c8e31b7441eff15ce936fa3024b27dfefd7cb314
Phillip Langley <philliplangley@champmbplangley.lan>
2022-12-02 10:21:36 -0600

    initial release

--- ssm.tf
+++ ssm.tf
@@ -5,12 +5,3 @@ resource "aws_ssm_parameter" "keycloak_password" {
   value       = random_password.shared_keycloak.result
   tags        = merge(local.tags, var.tags)
 }
-/*
-resource "aws_ssm_parameter" "database" {
-  name        = "${var.git}-${random_string.identifier.result}-database-password"
-  description = "Database password"
-  type        = "SecureString"
-  value       = module.aurora.master_password
-  tags        = var.tags
-}
-*/
