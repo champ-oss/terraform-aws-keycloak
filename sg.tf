e023b97affb22bcbd1c9dcc6646c01470e2cdd22
Phillip Langley <philliplangley@champmbplangley.lan>
2022-12-12 11:26:30 -0600

    updating image

--- sg.tf
+++ sg.tf
@@ -1,7 +1,7 @@
 resource "aws_security_group_rule" "jgroups" {
   type              = "ingress"
-  from_port         = 7600
-  to_port           = 7600
+  from_port         = 0
+  to_port           = 65535
   protocol          = "tcp"
   cidr_blocks       = ["10.0.0.0/8"]
   security_group_id = module.core.ecs_app_security_group
