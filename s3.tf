1a1a6f9a6f7be99f50245c052cb1c09889a461cc
Phillip Langley <philliplangley@champmbplangley.lan>
2022-12-12 16:54:18 -0600

    testing native s3 ping

--- s3.tf
+++ s3.tf
@@ -1,5 +1,5 @@
 module "s3" {
   source  = "github.com/champ-oss/terraform-aws-s3.git?ref=v1.0.35-1940fd8"
-  git     = local.git
+  git     = var.git
   protect = false # disabled just for testing
 }
