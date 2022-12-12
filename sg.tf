5e48330d7bc7abf92b918d4c41b3bc72d4d7e1d3
Phillip Langley <philliplangley@champmbplangley.lan>
2022-12-12 12:00:03 -0600

    testing out sg with self arg

--- sg.tf
+++ sg.tf
@@ -1,8 +1,8 @@
 resource "aws_security_group_rule" "jgroups" {
-  type              = "ingress"
+  type              = "group access"
   from_port         = 0
   to_port           = 65535
   protocol          = "tcp"
-  cidr_blocks       = ["10.0.0.0/8"]
+  self              = true
   security_group_id = module.core.ecs_app_security_group
 }
