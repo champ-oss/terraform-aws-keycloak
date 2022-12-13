0d1c5b6acff8201d25f60436eb7e8b45c2bbb9ec
Phillip Langley <philliplangley@champmbplangley.lan>
2022-12-12 21:53:39 -0600

    using s3 ping process

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -63,5 +63,4 @@ module "this" {
   zone_id             = data.aws_route53_zone.this.zone_id
   protect             = false
   skip_final_snapshot = true
-  app_command         = ["start"]
 }
--- sg.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_security_group_rule" "jgroups" {
-  type              = "ingress"
-  from_port         = 0
-  to_port           = 65535
-  protocol          = "tcp"
-  self              = true
-  security_group_id = module.core.ecs_app_security_group
-}
