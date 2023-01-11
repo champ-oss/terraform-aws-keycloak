b403c88683def69dd145cbbca8d3ef398e80ef99
Phillip Langley <philliplangley@champmbplangley.lan>
2023-01-11 12:55:07 -0600

    syntax issue

--- local-exec.tf
+++ local-exec.tf
@@ -2,7 +2,7 @@ resource "null_resource" "exec_create_client_script" {
 
   provisioner "local-exec" {
 
-    command     = "sleep 300;chmod +x ${path.module}/create-client.sh;${path.module}/create-client.sh"
+    command     = "sleep 300 && chmod +x ${path.module}/create-client.sh;${path.module}/create-client.sh"
     interpreter = ["bash", "-c"]
     environment = {
       KC_HOSTNAME             = local.keycloak_dns_name
