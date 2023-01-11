2e4b92f21a256f27bd7c8133dac25649b801f4dc
Phillip Langley <philliplangley@champmbplangley.lan>
2023-01-11 15:41:22 -0600

    updating tf client

--- local-exec.tf
+++ local-exec.tf
@@ -5,9 +5,9 @@ resource "null_resource" "exec_create_client_script" {
     command     = "sleep 300 && chmod +x ${path.module}/create-client.sh;${path.module}/create-client.sh"
     interpreter = ["bash", "-c"]
     environment = {
-      KC_HOSTNAME             = local.keycloak_dns_name
+      KC_HOSTNAME             = "https://${local.keycloak_dns_name}"
       KEYCLOAK_ADMIN          = var.keycloak_admin_user
-      KEYCLOAK_ADMIN_PASSWORD = "true"
+      KEYCLOAK_ADMIN_PASSWORD = aws_ssm_parameter.keycloak_password.name
       KEYCLOAK_CLIENT_ID      = "terraform-client"
       KEYCLOAK_CLIENT_SECRET  = random_password.keycloak_client_secret.result
     }
