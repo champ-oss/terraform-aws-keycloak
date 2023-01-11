08195ac05c4d4a55d531da506088da3e51293614
Phillip Langley <philliplangley@champmbplangley.lan>
2023-01-11 12:12:10 -0600

    syntax issue

--- local-exec.tf
+++ local-exec.tf
@@ -2,13 +2,13 @@ resource "null_resource" "exec_create_client_script" {
 
   provisioner "local-exec" {
 
-    command     = "sleep 45;chmod +x ${path.module}/create-client.sh;${path.module}/create-client.sh"
+    command     = "sleep 300;chmod +x ${path.module}/create-client.sh;${path.module}/create-client.sh"
     interpreter = ["bash", "-c"]
     environment = {
       KC_HOSTNAME             = local.keycloak_dns_name
       KEYCLOAK_ADMIN          = var.keycloak_admin_user
       KEYCLOAK_ADMIN_PASSWORD = "true"
-      KEYCLOAK_CLIENT_ID      = "admin-cli"
+      KEYCLOAK_CLIENT_ID      = "terraform-client"
       KEYCLOAK_CLIENT_SECRET  = random_password.keycloak_client_secret.result
     }
   }
