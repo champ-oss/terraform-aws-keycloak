831121cbca6204e8af59c0a2945fb20304d09d2d
Phillip Langley <philliplangley@champmbplangley.lan>
2023-01-11 16:36:26 -0600

    testing updated change

--- local-exec.tf
+++ local-exec.tf
@@ -7,7 +7,7 @@ resource "null_resource" "exec_create_client_script" {
     environment = {
       KC_HOSTNAME             = "https://${local.keycloak_dns_name}"
       KEYCLOAK_ADMIN          = var.keycloak_admin_user
-      KEYCLOAK_ADMIN_PASSWORD = aws_ssm_parameter.keycloak_password.name
+      KEYCLOAK_ADMIN_PASSWORD = random_password.shared_keycloak.result
       KEYCLOAK_CLIENT_ID      = "terraform-client"
       KEYCLOAK_CLIENT_SECRET  = random_password.keycloak_client_secret.result
     }
