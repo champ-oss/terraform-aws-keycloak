63e9427e3132ebaad22a3be975f07fb0b111693b
Phillip Langley <philliplangley@champmbplangley.lan>
2023-01-13 11:30:23 -0600

    testing new realm setup

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -70,8 +70,8 @@ data "aws_ssm_parameter" "keycloak" {
   name = "keycloak_client_secret"
 }
 
-module "keycloak" {
-  source = "github.com/champ-oss/terraform-keycloak.git?ref=fdf79f0de3653d1694a0ea4591e301e3dee0db51"
+module "keycloak_test" {
+  source        = "github.com/champ-oss/terraform-keycloak.git?ref=fdf79f0de3653d1694a0ea4591e301e3dee0db51"
   client_id     = "terraform-client"
   client_secret = data.aws_ssm_parameter.keycloak.value
   url           = module.this.keycloak_endpoint
