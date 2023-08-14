1a11dbce629d86d02223db1d37a80630cc48336b
Justin Reeves <reeves122@gmail.com>
2023-08-14 14:00:30 -0500

    add dms variables

--- variables.tf
+++ variables.tf
@@ -180,3 +180,15 @@ variable "enable_cluster" {
   default     = true
   type        = bool
 }
+
+variable "create_dms_endpoint" {
+  description = "https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/dms_endpoint"
+  type        = bool
+  default     = false
+}
+
+variable "dms_endpoint_type" {
+  description = "https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/dms_endpoint#endpoint_type"
+  type        = string
+  default     = "source"
+}
